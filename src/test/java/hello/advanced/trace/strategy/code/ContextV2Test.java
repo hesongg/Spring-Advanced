d0063689e5d707b6ce7da56f54c9206efeec77c4
hesung <hsjo6864@gmail.com>
2022-08-31 01:08:27 +0900

    202.08.29 스프링 핵심 원리 파악 프로젝트 - 전략 패턴 테스트 코드작성 - 파라미터 방식  - 필드가 아닌 파라미터로 전략을 주입받아서 context가 실행될때마다 다른 전략이 수행될 수 있도록 구현함

---
+++ src/test/java/hello/advanced/trace/strategy/code/ContextV2Test.java
@@ -0,0 +1,20 @@
+package hello.advanced.trace.strategy.code;
+
+import hello.advanced.trace.strategy.code.strategy.ContextV2;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+
+    /**
+     * 전략 패턴 적용
+     */
+    @Test
+    void strategyV1() {
+        ContextV2 context = new ContextV2();
+        context.execute(() -> log.info("비즈니스 로직1 실행"));
+        context.execute(new StrategyLogic2());
+    }
+}
